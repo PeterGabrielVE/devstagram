360c7c6524cbc1a2da91ea02eafe622eb7f26bee
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-10-03 21:37:55 -0400

    tailwind

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Devstagram</title>
-
+    @vite('resources/css/app.css')
 </head>
 <body>
     <nav>
@@ -14,7 +14,7 @@
         <a href="/tienda">Tienda</a>
     </nav>
 
-    <h1>@yield('titulo')</h1>
+    <h1 class="text-4xl">@yield('titulo')</h1>
     <hr>
     @yield('contenido')
 </body>
