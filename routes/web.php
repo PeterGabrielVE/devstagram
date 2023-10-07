37e6f103d54b73af5f59d04bf803c0ab0ecbae02
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-10-06 22:30:26 -0400

    register controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,5 +18,6 @@ Route::get('/', function () {
     return view('main');
 });
 
+Route::get('/crear-cuenta',[RegisterController::class, 'index']);
 
 
