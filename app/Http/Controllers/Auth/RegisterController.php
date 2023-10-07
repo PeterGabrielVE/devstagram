c3cdd66da27445fcbc22be747da17c21c10e2fac
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-10-07 17:38:04 -0400

    leer datos

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -11,7 +11,7 @@ class RegisterController extends Controller
         return view('auth.register');
     }
 
-    public function store(){
+    public function store(Request $request){
        dd('hiii');
     }
 }
